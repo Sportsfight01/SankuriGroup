f6ca07f9bf2b68436686bd1e5f5f9c6ae187478a
naveenrajDMSS <85547199+naveenrajDMSS@users.noreply.github.com>
2025-11-21 10:51:04 +0530

    Initial Commit

---
+++ SankuriGroup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SankuriGroup
+//
+//  Created by Naveen Kourampalli on 21/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
